41793f1fb9fe8e4bb2a50ad35a5ed844f9cf49e2
mateus <mateusmacedoeng@gmail.com>
2025-09-28 18:17:57 -0300

    add describe for test and changing from test to it

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,22 +1,24 @@
 import request from 'supertest';
-import { afterAll, beforeAll, test } from 'vitest';
+import { afterAll, beforeAll, describe, it } from 'vitest';
 import { app } from '../src/app';
 
-beforeAll(async () => {
-    await app.ready();
-});
+describe('Transactions routes', () => {
+    beforeAll(async () => {
+        await app.ready();
+    });
 
-afterAll(async () => {
-    await app.close();
-});
+    afterAll(async () => {
+        await app.close();
+    });
 
-test('User can create a new transaction', async () => {
-    const response = await request(app.server)
-        .post('/transactions')
-        .send({
-            title: 'New transaction',
-            amount: 5000,
-            type: 'credit',
-        })
-        .expect(201);
+    it('should be able to create a new transaction', async () => {
+        const response = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit',
+            })
+            .expect(201);
+    });
 });
